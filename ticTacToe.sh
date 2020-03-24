4cbb74ea9a8f9c2af70f95426badf905eab08264
sujitbidawe <bidawesujit@gmail.com>
2020-03-25 04:58:08 +0530

    added: code to decide who will play first

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -7,13 +7,11 @@ board=(0 1 2 3 4 5 6 7 8)
 
 function resetBoard() {
 	echo "New game starts"
-
 	board=(0 1 2 3 4 5 6 7 8)
-	
 }
 
 function displayBoard(){
-for (( row=0; row<7; row=row+3 ))
+for (( row=0; row<=6; row=row+3 ))
 do
 	echo " ${board[$row]} | ${board[$row+1]} | ${board[$row+2]}"
 	if [ $row -lt 6 ]
@@ -24,33 +22,40 @@ done
 }
 
 function letterAssign(){
-
 	if [[ $((RANDOM%2)) -eq 0 ]]
 	then	
-		currentPlayer="user"
 		user="X"
 		computer="O"
 	else
-		currentPlayer="computer"
 		computer="X"
 		user="O"
 	fi
-
 	echo "user will play with $user and computer will play with $computer"
+}
 
+function toss(){
+	if [[ $((RANDOM%2)) -eq 0 ]]
+	then
+		currentPlayer="user"
+	else
+		currentPlayer="computer"
+	fi
+	echo "$currentPlayer will start the game!"
 }
 
 function main(){
-
 	read -p "Do you want to start the game? enter 'y' for yes or anything else for no: " choice
 	
 	if [[ $choice = "y" || $choice = "Y" ]]
 	then
 		resetBoard
 		displayBoard
+		letterAssign
+		toss
+	else
+		echo "Bye!"
 	fi
 
-	letterAssign
 }
 
 main
