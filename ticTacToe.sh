81a50b9c28f40105f53fbd324115b1d2b2dcfc59
sujitbidawe <bidawesujit@gmail.com>
2020-03-25 06:50:02 +0530

    added: code to check winner or tie after every move and change player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,12 +1,9 @@
 #!/usr/local/bin/bash
 
-clear 
-
-declare -a board
-board=(0 1 2 3 4 5 6 7 8)
+clear
 
 function resetBoard() {
-	echo "New game starts"
+	echo "********** New game starts **********"
 	board=(0 1 2 3 4 5 6 7 8)
 }
 
@@ -19,6 +16,7 @@ do
 		echo "---*---*---"
 	fi 
 done
+echo ""
 }
 
 function letterAssign(){
@@ -43,18 +41,105 @@ function toss(){
 	echo "$currentPlayer will start the game!"
 }
 
+function winChecker() {
+   diagonal=0
+   column=0
+   for((row=0;row<9;row=row+3))
+   do
+      if [[ ${board[$row]} == ${board[$row+1]} && ${board[$row+1]} == ${board[$row+2]} ]] ||
+         [[ ${board[$column]} == ${board[$column+3]} && ${board[$column+3]} ==  ${board[$column+6]} ]] || 
+         [[ ${board[$diagonal]} == ${board[$diagonal+4]} && ${board[$diagonal+4]} == ${board[$diagonal+8]} ]] ||
+         [[ ${board[$diagonal+2]} == ${board[$diagonal+4]} && ${board[$diagonal+4]} == ${board[$diagonal+6]} ]]
+      then  
+         echo "$currentPlayer Wins!!"
+         echo ""
+         main
+      fi
+      column=$((column+1))
+   done
+}
+
+
+function play(){
+	if [[ "$currentPlayer" = "user" ]]
+	then
+		userPlay
+	else
+		computerPlay
+	fi
+}
+
+function userPlay(){
+	currentPlayer="user"
+	if [[ $turnCount -lt $MAX_TURNS ]]
+	then
+		read -p "Choose your position between 0 to 8: " position
+		if [[ "${board[$position]}" = "$position" ]]
+		then
+			board[$position]=$user	
+			((turnCount++))
+			displayBoard
+		else
+			echo "Invalid position, please enter valid position between 0 to 8"
+			userPlay
+		fi
+		winChecker
+		computerPlay
+	else
+		echo "Game tie !!"
+		echo ""
+		main
+	fi
+}
+
+function computerPlay(){
+	currentPlayer="computer"
+	if [[ $turnCount -lt $MAX_TURNS ]]
+	then
+		position=$((RANDOM%9))
+		if [[ "${board[$position]}" = "$position" ]]
+		then
+			echo "computer's play:"
+			board[$position]=$computer
+			((turnCount++))
+			displayBoard
+		else
+			computerPlay
+		fi
+		winChecker
+		userPlay
+	else
+		echo "Game tie !!"
+		echo ""
+		main
+	fi
+}
+
+
 function main(){
-	read -p "Do you want to start the game? enter 'y' for yes or anything else for no: " choice
+	read -p "Do you want to start a game? enter 'y' for yes or anything else for no: " choice
 	
 	if [[ $choice = "y" || $choice = "Y" ]]
 	then
+		clear
+
+		declare -a board
+		
+		turnCount=0
+		MAX_TURNS=9
+		user=""
+		computer=""
+		currentPlayer=""
+		
 		resetBoard
 		letterAssign
 		toss
 		displayBoard
+		play
 
 	else
 		echo "Bye!"
+		exit
 	fi
 
 }
